c441b09a749d9ea3c02c94ffa8dc806b2a75ef52
Marcos <marcosdevelopment.business@gmail.com>
2024-03-29 21:50:29 -0300

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
-    <main className='w-full h-full bg-black flex items-center justify-center'>
+    <main className='w-full h-full bg-black flex flex-col items-center justify-center'>
       <a
         href='https://github.com/marcos-venicius'
         className='block'
@@ -8,6 +10,12 @@ export function App() {
         rel='noopener noreferrer'>
         <img src='/m.png' className='max-w-[512px] max-h-[512px] animate-pulse rounded-full' />
       </a>
+
+      <Button asChild>
+        <a href='https://ui.shadcn.com/docs' target='_blank' rel='noopener noreferrer'>
+          View shadcn docs
+        </a>
+      </Button>
     </main>
   )
 }
