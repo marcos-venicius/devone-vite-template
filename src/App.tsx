71965ada96d384f4a994556ff9b42376e21d676c
Marcos <marcosdevelopment.business@gmail.com>
2024-03-29 21:40:54 -0300

    feat: create template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <main className='w-full h-full bg-black flex items-center justify-center'>
+      <a
+        href='https://github.com/marcos-venicius'
+        className='block'
+        target='_blank'
+        rel='noopener noreferrer'>
+        <img src='/m.png' className='max-w-[512px] max-h-[512px] animate-pulse rounded-full' />
+      </a>
+    </main>
+  )
+}
